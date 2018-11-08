1fabb87e3b146aae286cf1ee22e35bde873e7fb9
Ray <rayliu2007@gmail.com>
2018-11-08 00:43:12 -0500

    zoo

--- cleverhans/attacks_tf.py
+++ cleverhans/attacks_tf.py
@@ -1029,7 +1029,7 @@ class Zoo:
         self.output = self.model.get_probs(self.newimg)
 
         # distance to the input data
-        self.l2dist = tf.reduce_sum(tf.square(self.newimg - self.timg), [1, 2, 3]) + tf.reduce_sum(tf.square(self.model.get_encoded(self.newimg) - self.model.get_encoded(self.timg)), [1, 2, 3])
+        self.l2dist = tf.reduce_sum(tf.square(self.newimg - self.timg), [1, 2, 3]) + tf.reduce_sum(tf.square(self.modelAE.get_encoded(self.newimg) - self.modelAE.get_encoded(self.timg)), [1, 2, 3])
 
         # compute the probability of the label class versus the maximum other
         # self.tlab * self.output selects the Z value of real class
